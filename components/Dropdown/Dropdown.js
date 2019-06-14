efa76fb44f19491c07b15f3e9e488fabac2a39fa
elisabethearley <elisabethearley@gmail.com>
2019-06-13 22:00:02 -0400

    assigned .tabs-link to the variable 'links', added .forEach and returned new TabLink on Tabs.js, completed constructor class for TabLink by assigning 'element', got data attribute .tab, referenced the .tabs-item to grab thethis.data, created new instance of TabItem, added 'click' eventListener and called 'select' method, created select() by assigning .tabs-link to 'links', used .forEach to remove .tabs-link-selected, add class named .tabs-link-selected and called select() on 'tabItem', created sub-constructor TabItem class by assigning element, completing select() with .tabs-item assigned to 'items', used .from().forEach() to remove .tabs-item-selected, added .tabs-item-selected to element

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,10 +5,10 @@ class Dropdown {
     this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector('.dropdown');
+    this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.button.querySelector('.dropdown-content');
+    this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
@@ -25,4 +25,5 @@ class Dropdown {
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => 
+  new Dropdown(dropdown));
